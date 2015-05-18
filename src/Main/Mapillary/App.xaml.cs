ae44a2de65be55ece2c93a6f97e564a767ccb692
Tommy Ovesen <tommy@ovesen.net>
2015-05-18 19:38:17 +0200

    Animated transitions

--- src/Main/Mapillary/App.xaml.cs
+++ src/Main/Mapillary/App.xaml.cs
@@ -13,18 +13,15 @@ using System.Collections.Generic;
 
 namespace Mapillary
 {
+    /* Mapillary App for Windows Phone. Developed By Tommy Ovesen, ***REMOVED***, http://www.facebook.com/ovesen.net */
     public partial class App : Application
     {
-        /// <summary>
-        /// Provides easy access to the root frame of the Phone Application.
-        /// </summary>
-        /// <returns>The root frame of the Phone Application.</returns>
         public static DateTime ProfileLastRefreshed { get; set; }
         public static PhoneApplicationFrame RootFrame { get; private set; }
         public static int CaptureInterval { get; set; }
         public static bool ShutterSoundEnabled { get; set; }
         public static bool SaveToCameraRollEnabled { get; set; }
-        public static string WP_CLIENT_ID = "<key>";
+        public static string WP_CLIENT_ID = "MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzo0ZmYxN2MzMTRlYzM1M2E2";
         public static Windows.Devices.Geolocation.Geolocator GeoLocator { get; set; }
         public static List<ImgSequence> SequenceListCache { get; set; }
         public static List<FeedItem> EventListCache { get; set; }
@@ -32,22 +29,14 @@ namespace Mapillary
         public static string NumMetersProfileCache { get; set; }
         public static string NumPhotosProfileCache { get; set; }
         public static string AboutProfileCache { get; set; }
+        public static DateTime FeedLastRefreshed { get; set; }
         public static System.Windows.Media.ImageSource ImageProfileCache { get; set; }
         public App()
         {
-            // Global handler for uncaught exceptions.
             UnhandledException += Application_UnhandledException;
-
-            // Standard XAML initialization
             InitializeComponent();
-
-            // Phone-specific initialization
             InitializePhoneApplication();
-
-            // Language display initialization
             InitializeLanguage();
-
-            // Show graphics profiling information while debugging.
             Application.Current.Host.Settings.EnableFrameRateCounter = false;
 
             if (Debugger.IsAttached)
@@ -121,8 +110,7 @@ namespace Mapillary
                 PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Enabled;
             }
         }
-        // Code to execute when the application is launching (eg, from Start)
-        // This code will not execute when the application is reactivated
+
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
             GpsIsReady = false;
@@ -133,8 +121,6 @@ namespace Mapillary
             InitGps();
         }
 
-        // Code to execute when the application is activated (brought to foreground)
-        // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
             GpsIsReady = false;
@@ -144,21 +130,16 @@ namespace Mapillary
             InitGps();
         }
 
-        // Code to execute when the application is deactivated (sent to background)
-        // This code will not execute when the application is closing
         private void Application_Deactivated(object sender, DeactivatedEventArgs e)
         {
             StopGps();
         }
 
-        // Code to execute when the application is closing (eg, user hit Back)
-        // This code will not execute when the application is deactivated
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
             StopGps();
         }
 
-        // Code to execute if a navigation fails
         private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             if (Debugger.IsAttached)
@@ -168,12 +149,10 @@ namespace Mapillary
             }
         }
 
-        // Code to execute on Unhandled Exceptions
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
         {
             if (Debugger.IsAttached)
             {
-                // An unhandled exception has occurred; break into the debugger
                 Debugger.Break();
             }
             else
@@ -206,56 +185,40 @@ namespace Mapillary
             }
         }
 
-        // Do not add any additional code to this method
         private void InitializePhoneApplication()
         {
             if (phoneApplicationInitialized)
                 return;
 
-            // Create the frame but don't set it as RootVisual yet; this allows the splash
-            // screen to remain active until the application is ready to render.
-            RootFrame = new PhoneApplicationFrame();
+            RootFrame = new TransitionFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
-
-            // Handle navigation failures
             RootFrame.NavigationFailed += RootFrame_NavigationFailed;
-
-            // Handle reset requests for clearing the backstack
             RootFrame.Navigated += CheckForResetNavigation;
-
-            // Ensure we don't initialize again
             phoneApplicationInitialized = true;
         }
 
-        // Do not add any additional code to this method
         private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
         {
-            // Set the root visual to allow the application to render
             if (RootVisual != RootFrame)
+            {
                 RootVisual = RootFrame;
+            }
 
-            // Remove this handler since it is no longer needed
             RootFrame.Navigated -= CompleteInitializePhoneApplication;
         }
 
         private void CheckForResetNavigation(object sender, NavigationEventArgs e)
         {
-            // If the app has received a 'reset' navigation, then we need to check
-            // on the next navigation to see if the page stack should be reset
             if (e.NavigationMode == NavigationMode.Reset)
                 RootFrame.Navigated += ClearBackStackAfterReset;
         }
 
         private void ClearBackStackAfterReset(object sender, NavigationEventArgs e)
         {
-            // Unregister the event so it doesn't get called again
             RootFrame.Navigated -= ClearBackStackAfterReset;
 
-            // Only clear the stack for 'new' (forward) and 'refresh' navigations
             if (e.NavigationMode != NavigationMode.New && e.NavigationMode != NavigationMode.Refresh)
                 return;
-
-            // For UI consistency, clear the entire page stack
             while (RootFrame.RemoveBackEntry() != null)
             {
                 ; // do nothing
@@ -264,52 +227,18 @@ namespace Mapillary
 
         #endregion
 
-        // Initialize the app's font and flow direction as defined in its localized resource strings.
-        //
-        // To ensure that the font of your application is aligned with its supported languages and that the
-        // FlowDirection for each of those languages follows its traditional direction, ResourceLanguage
-        // and ResourceFlowDirection should be initialized in each resx file to match these values with that
-        // file's culture. For example:
-        //
-        // AppResources.es-ES.resx
-        //    ResourceLanguage's value should be "es-ES"
-        //    ResourceFlowDirection's value should be "LeftToRight"
-        //
-        // AppResources.ar-SA.resx
-        //     ResourceLanguage's value should be "ar-SA"
-        //     ResourceFlowDirection's value should be "RightToLeft"
-        //
-        // For more info on localizing Windows Phone apps see http://go.microsoft.com/fwlink/?LinkId=262072.
-        //
+
         private void InitializeLanguage()
         {
             try
             {
-                // Set the font to match the display language defined by the
-                // ResourceLanguage resource string for each supported language.
-                //
-                // Fall back to the font of the neutral language if the Display
-                // language of the phone is not supported.
-                //
-                // If a compiler error is hit then ResourceLanguage is missing from
-                // the resource file.
-                RootFrame.Language = XmlLanguage.GetLanguage(AppResources.ResourceLanguage);
 
-                // Set the FlowDirection of all elements under the root frame based
-                // on the ResourceFlowDirection resource string for each
-                // supported language.
-                //
-                // If a compiler error is hit then ResourceFlowDirection is missing from
-                // the resource file.
+                RootFrame.Language = XmlLanguage.GetLanguage(AppResources.ResourceLanguage);
                 FlowDirection flow = (FlowDirection)Enum.Parse(typeof(FlowDirection), AppResources.ResourceFlowDirection);
                 RootFrame.FlowDirection = flow;
             }
             catch
             {
-                // If an exception is caught here it is most likely due to either
-                // ResourceLangauge not being correctly set to a supported language
-                // code or ResourceFlowDirection is set to a value other than LeftToRight
-                // or RightToLeft.
 
                 if (Debugger.IsAttached)
                 {
@@ -319,7 +248,5 @@ namespace Mapillary
                 throw;
             }
         }
-
-        public static DateTime FeedLastRefreshed { get; set; }
     }
 }
